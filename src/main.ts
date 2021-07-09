ff6605faca24616d738cd7356f90df98950c4153
Akramurridjal Rahman <xerod@outlook.com>
2021-07-09 13:24:38 +0700

    First Commit

---
+++ src/main.ts
@@ -0,0 +1,14 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import { createRouter, createWebHistory } from "vue-router";
+import routes from "voie-pages";
+import "virtual:windi.css";
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes,
+});
+
+const app = createApp(App).use(router);
+
+app.mount("#app");
